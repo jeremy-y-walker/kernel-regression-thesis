02268c7173af7389e3285cae4adb11f167722436
jeremy-y-walker <85331297+jeremy-y-walker@users.noreply.github.com>
2021-07-09 14:42:18 -0700

    Update Bootstrap Function.R

--- Bootstrap Function.R
+++ Bootstrap Function.R
@@ -1,3 +1,16 @@
+#This function uses boostrapping to replicate the estimates from the Nadaraya-Watson mixed data type nonparametric regression
+#model implemented in R with the np package. The argument data should be a dataframe to accomodate mixed data types (nominal,
+#ordinal, continuous) with rows corresponding to observations. bw_object should be an object of class 'rbandwidth' created
+#with the np package function npregbw which is then used to define the npreg (nonparametric regression model) of which you want
+#to estimate the standard errors of. B is the number of replications you would like (heads up; this is a computationally intensive task).
+#The function returns a B x nrow(data) matrix where each row is one replication of all points of estimation; to estimate standard error
+#apply the sd function along each column.
+
+#Note: A bootstrap replication will omit ~36% of data so this function can also establish robustness by creating a "train/test split"
+#for each replication"
+
+library('np')
+
 kernel_reg_boostrap = function(data, bw_object, B) {
   mean_eval = matrix(NA, nrow = B, ncol = nrow(data))
   
